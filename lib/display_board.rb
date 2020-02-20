a1a887df93c9fb9f7efa0ccb805b16fff2d96eef
Michelle Hauser <michellehauser5@gmail.com>
2020-02-20 04:38:25 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,6 +2,15 @@
 # out the current state.
 board=[" "," "," "," "," "," "," "," "," "]
 
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]}"
   puts "-----------"
